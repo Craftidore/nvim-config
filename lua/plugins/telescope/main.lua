66d30a941caa45a1f0a61203196a5736bf6645b2
Craftidore <elliot@madbear.com>
2023-02-28 07:51:30 -0600

    Added telescope <leader>fd

--- lua/plugins/telescope/main.lua
+++ lua/plugins/telescope/main.lua
@@ -5,10 +5,12 @@ require('packerList').add({
     config = function ()
         local api = vim.api
         local opts = { noremap = true }
-        api.nvim_set_keymap("n", "<leader>ff", [[<cmd>Telescope find_files<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fg", [[<cmd>Telescope live_grep<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fb", [[<cmd>Telescope buffers<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fh", [[<cmd>Telescope help_tags<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>ff", [[:Telescope find_files<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>fg", [[:Telescope live_grep<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>fb", [[:Telescope buffers<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>fd", [[:Telescope command_history<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>ft", [[:Telescope treesitter<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>fh", [[:Telescope help_tags<cr>]], opts)
     end
 })
 
