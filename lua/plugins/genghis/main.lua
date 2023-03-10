5ec2e06560656daad9dd28ee6ef5301f46ac28b2
Craftidore <elliot@madbear.com>
2023-03-10 12:58:35 -0600

    Telescope & genghis hotkey changes

--- lua/plugins/genghis/main.lua
+++ lua/plugins/genghis/main.lua
@@ -2,13 +2,15 @@ require('packerList').add({'chrisgrieser/nvim-genghis', config = function ()
     local genghis = require "genghis"
 
     local keymap = vim.keymap.set
-    keymap("n", "<leader>fyp", genghis.copyFilepath)
-    keymap("n", "<leader>fyn", genghis.copyFilename)
-    keymap("n", "<leader>fcx", genghis.chmodx)
-    keymap("n", "<leader>frr", genghis.renameFile)
-    keymap("n", "<leader>fnn", genghis.createNewFile)
-    keymap("n", "<leader>fyy", genghis.duplicateFile)
-    keymap("n", "<leader>fdd", genghis.trashFile) -- requires macOS or Linux `mv` command
+    -- NOTE: All starting with 'e' because genghis is inspired by __e__unuch
+    --       https://github.com/chrisgrieser/nvim-genghis#how-is-this-different-from-vimeunuch
+    keymap("n", "<leader>eyp", genghis.copyFilepath)
+    keymap("n", "<leader>eyn", genghis.copyFilename)
+    keymap("n", "<leader>eyy", genghis.duplicateFile)
+    keymap("n", "<leader>ecx", genghis.chmodx)
+    keymap("n", "<leader>err", genghis.renameFile)
+    keymap("n", "<leader>en", genghis.createNewFile)
+    keymap("n", "<leader>ed", genghis.trashFile) -- requires macOS or Linux `mv` command
     keymap("x", "<leader>x", genghis.moveSelectionToNewFile)
 end})
 
--- lua/plugins/telescope/main.lua
+++ lua/plugins/telescope/main.lua
@@ -1,16 +1,24 @@
-
 require('packerList').add({
     'nvim-telescope/telescope.nvim', tag = '0.1.0', -- fuzzy file finder
     requires = { {'nvim-lua/plenary.nvim'} }, -- pre-defined functions that other things depend on
     config = function ()
         local api = vim.api
         local opts = { noremap = true }
-        api.nvim_set_keymap("n", "<leader>ff", [[:Telescope find_files<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fg", [[:Telescope live_grep<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fb", [[:Telescope buffers<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fd", [[:Telescope command_history<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>ft", [[:Telescope treesitter<cr>]], opts)
-        api.nvim_set_keymap("n", "<leader>fh", [[:Telescope help_tags<cr>]], opts)
+        -- space-space-t for default Telescope search
+        api.nvim_set_keymap("n", "<leader><leader>t", [[<cmd>Telescope<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tt", [[<cmd>Telescope treesitter<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tf", [[<cmd>Telescope find_files<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tg", [[<cmd>Telescope live_grep<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tb", [[<cmd>Telescope buffers<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>th", [[<cmd>Telescope help_tags<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tn", [[<cmd>Telescope find_files cwd=~/.config/nvim/<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>tk", [[<cmd>Telescope keymaps<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>gf", [[<cmd>Telescope git_files<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>gb", [[<cmd>Telescope git_branches<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>lr", [[<cmd>Telescope lsp_references<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>ls", [[<cmd>Telescope lsp_document_symbols<cr>]], opts)
+        api.nvim_set_keymap("n", "<leader>le", [[<cmd>Telescope diagnostic<cr>]], opts) -- lsp_errors
+        -- api.nvim_set_keymap("n", "<leader>tc", [[<cmd>Telescope command_history<cr>]], opts)
     end
 })
 
