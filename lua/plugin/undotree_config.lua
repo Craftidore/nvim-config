45a2b164ba8226b3ced894373feee4039508df66
craftidore <elliot@madbear.com>
2025-06-18 11:02:11 -0500

    Undotree ignores mini-starter

--- lua/plugin/undotree_config.lua
+++ lua/plugin/undotree_config.lua
@@ -1,7 +1,33 @@
 local UndoTree = {
   'jiaoshijie/undotree',
   dependencies = 'nvim-lua/plenary.nvim',
-  opts = {},
+  opts = {
+    float_diff = true, -- using float window previews diff, set this `true` will disable layout option
+    layout = 'left_bottom', -- "left_bottom", "left_left_bottom"
+    position = 'left', -- "right", "bottom"
+    ignore_filetype = {
+      'ministarter',
+      'undotree',
+      'undotreeDiff',
+      'qf',
+      'TelescopePrompt',
+      'spectre_panel',
+      'tsplayground',
+    },
+    window = {
+      winblend = 30,
+    },
+    keymaps = {
+      ['j'] = 'move_next',
+      ['k'] = 'move_prev',
+      ['gj'] = 'move2parent',
+      ['J'] = 'move_change_next',
+      ['K'] = 'move_change_prev',
+      ['<cr>'] = 'action_enter',
+      ['p'] = 'enter_diffbuf',
+      ['q'] = 'quit',
+    },
+  },
   keys = { -- load the plugin only when using it's keybinding:
     { '<leader>ou', "<cmd>lua require('undotree').toggle()<cr>", mode = 'n', desc = '[O]pen [U]ndotree' },
   },
