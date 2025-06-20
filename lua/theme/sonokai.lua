ea8bb9b6f8333b06b2b66c425590f99d472a16c3
craftidore <elliot@madbear.com>
2025-06-20 09:21:13 -0500

    Sonokai uses init over config

--- lua/theme/sonokai.lua
+++ lua/theme/sonokai.lua
@@ -2,10 +2,7 @@ local SonokaiConfig = {
   'sainnhe/sonokai',
   lazy = false,
   priority = 1000,
-  opts = {},
-  config = function()
-    -- Temporary disable-background-modifications to keep primary and new neovim configs distinct
-    -- -- vim.cmd([[autocmd ColorScheme * highlight Normal ctermbg=NONE guibg=NONE]])
+  init = function()
     vim.g.sonokai_style = 'espresso'
     vim.g.sonokai_enable_italic = true
     vim.cmd.colorscheme('sonokai')
