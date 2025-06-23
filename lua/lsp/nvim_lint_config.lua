2241a8261163bcedac7e1445b838f18be801cbf4
Craftidore <elliot@madbear.com>
2025-06-23 13:23:29 -0500

    Nvim Lint ignores help docs

--- lua/lsp/nvim_lint_config.lua
+++ lua/lsp/nvim_lint_config.lua
@@ -1,3 +1,13 @@
+local function lint_ignore(ft)
+  local ignored_filetypes = {
+    'help',
+    'ministarter',
+  }
+  local contains = vim.g.utils.lua.contains
+  local bad_ft = contains(ignored_filetypes, ft)
+  return bad_ft
+end
+
 local NvimLint = {
   'mfussenegger/nvim-lint',
   lazy = false,
@@ -9,7 +19,12 @@ local NvimLint = {
   end,
   init = function()
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost' }, {
-      callback = function()
+      callback = function(ev)
+        local filetype = vim.api.nvim_get_option_value('filetype', { buf = ev.buf })
+        if lint_ignore(filetype) then
+          return
+        end
+
         local lint = require('lint')
         -- Let this fail silently, since linters may not be installed yet
         xpcall(function()
--- lua/utils/lua.lua
+++ lua/utils/lua.lua
@@ -24,4 +24,19 @@ Lua.kfilter = function(t, filterIter)
   return out
 end
 
+-- Shamelessly adapted from https://www.reddit.com/r/learnprogramming/comments/s41ykx/lua_is_there_a_method_to_see_if_a_table_or_array/
+Lua.contains = function(arr, value, test)
+  if test == nil then
+    test = function(lhs, rhs)
+      return lhs == rhs
+    end
+  end
+  for i = 1, #arr do
+    if test(arr[i], value) then
+      return true
+    end
+  end
+  return false
+end
+
 return Lua
