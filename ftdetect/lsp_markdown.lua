52815cb4f9fee9be74a011d6a5178be87e60e780
Craftidore <elliot@madbear.com>
2025-07-10 13:06:18 -0500

    render markdown plugin added; thanks breadonpenguins

--- ftdetect/lsp_markdown.lua
+++ ftdetect/lsp_markdown.lua
@@ -1,7 +1,8 @@
 -- Normal 'markdown' filetype doesn't highlight `code` right
-vim.filetype.add({
-  extension = {
-    -- NOTE: Seems like the default priority for user filetype is lower than 'markdown'
-    ['md'] = 'lsp_markdown',
-  },
-})
+-- NOTE: Update: Fixed code highlighting with render_markdown plugin
+-- vim.filetype.add({
+--   extension = {
+--     -- NOTE: Seems like the default priority for user filetype is lower than 'markdown'
+--     ['md'] = 'lsp_markdown',
+--   },
+-- })
