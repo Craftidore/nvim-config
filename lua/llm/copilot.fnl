54121c04b931fac2efb9e3da54a18a182f29c84c
Craftidore <elliot@madbear.com>
2026-04-22 09:11:24 -0500

    Guard copilot.fnl behind is_machmotion

--- lua/llm/copilot.fnl
+++ lua/llm/copilot.fnl
@@ -1,5 +1,8 @@
 (local vim _G.vim)
+(local utils vim.g.utils)
 
-{ 1 :github/copilot.vim
-    :config (fn [plugin opts]
-              (vim.cmd "Copilot disable")) }
+(if (utils.is_machmotion)
+  { 1 :github/copilot.vim
+      :config (fn [plugin opts]
+                (vim.cmd "Copilot disable")) }
+  {})
--- lua/llm/copilot.lua
+++ lua/llm/copilot.lua
@@ -1,6 +1,11 @@
 -- [nfnl] lua/llm/copilot.fnl
 local vim = _G.vim
-local function _1_(plugin, opts)
-  return vim.cmd("Copilot disable")
+local utils = vim.g.utils
+if utils.is_machmotion() then
+  local function _1_(plugin, opts)
+    return vim.cmd("Copilot disable")
+  end
+  return {"github/copilot.vim", config = _1_}
+else
+  return {}
 end
-return {"github/copilot.vim", config = _1_}
